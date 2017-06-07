314c895565a7841004375ae7c63ae8d09acee8f4
moahmmad <malhowaidi@gmail.com>
2017-06-07 23:35:50 +0000

    first commit

---
+++ qClient.cpp
@@ -0,0 +1,84 @@
+#include <boost/interprocess/ipc/message_queue.hpp>
+#include <iostream>
+#include <vector>
+//#include "options.hpp"
+//#include "ndncatchunks.hpp"
+//#include "consumer.hpp"
+#include <chrono>
+#include <thread>
+#include <time.h>
+
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+//#include <cpprest/ppltasks.h>
+//#include <json.h>
+//#pragma comment(lib, "cpprest110_1_1")
+#include "cpprest/http_listener.h"
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+
+using namespace boost::interprocess;
+using namespace std;
+int main ()
+{
+ message_queue::remove("message_queue");
+// ndn::chunks::ndnChunks nchunks;
+    while(true)
+    {
+   try{
+//struct timespec tim,tim2;
+//tim.tv_sec = 0;
+//tim.tv_nsec = 1;
+//nanosleep(&tim, NULL);
+
+
+ std::this_thread::sleep_for(std::chrono::nanoseconds(10000));
+      message_queue mq(open_only ,"message_queue") ;
+
+      size_t const max_msg_size = 0x100;
+
+      std::string s;
+      s.resize(max_msg_size);
+      size_t msg_size;
+      unsigned msg_prio;
+      mq.receive(&s[0], s.size(), msg_size, msg_prio);
+      s.resize(msg_size);
+
+//      ndn::chunks::ndnChunks nchunks;
+//      nchunks.startChunk(s);
+
+
+      std::cout <<"s: " << s << std::endl;
+
+      // sending post message with the information .......
+      http_client client(U("http://10.71.103.63:34568"));
+
+
+      json::value postData;
+      postData["name"] = json::value::string(U(s));
+//      postData["work"] = json::value::string(U("Phd"));
+      http_request request;
+      request.set_body(postData);
+      request.set_method(methods::POST);
+
+      wcout << L"\nput values\n";
+      http_response response = client.request(request).get();
+      if(response.status_code() == status_codes::OK)
+      {
+	      auto body = response.extract_string();
+	      std::wcout << L"Added new Id: " << body.get().c_str() << std::endl;
+
+	      //      return std::stoi(body.get().c_str());
+      }
+      //...........................
+
+   }
+   catch(interprocess_exception &ex){
+	   message_queue::remove("message_queue");
+   }
+
+   message_queue::remove("message_queue");
+    }
+    return 0;
+}
